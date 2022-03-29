a335dd26cca866d034628be2d2464e97315bf58b
Manash Pratim Pathak <mppoff021@gmail.com>
2022-03-29 06:37:36 +0530

    revision done

--- 2. Find the maximum and minimum number in an array/FindTheMaxMinOfAnArray.cpp
+++ 2. Find the maximum and minimum number in an array/FindTheMaxMinOfAnArray.cpp
@@ -1,56 +1,114 @@
+// #include <iostream>
+
+// using namespace std;
+
+// struct Pair
+// {
+//     int max;
+//     int min;
+// };
+
+// Pair getMinMax(int arr[], int size)
+// {
+//     struct Pair minmax;
+//     if (size == 1)
+//     {
+//         minmax.max = arr[0];
+//         minmax.min = arr[0];
+//         return minmax;
+//     }
+
+//     if (arr[0] > arr[1])
+//     {
+//         minmax.max = arr[0];
+//         minmax.min = arr[1];
+//     }
+//     else if (arr[0] < arr[1])
+//     {
+//         minmax.max = arr[1];
+//         minmax.min = arr[0];
+//     }
+//     for (int i = 2; i < size; i++)
+//     {
+//         if (arr[i] > minmax.max)
+//         {
+//             minmax.max = arr[i];
+//         }
+//         else if(arr[i] < minmax.min)
+//         {
+//             minmax.min = arr[i];
+//         }
+//     }
+//     return minmax;
+// }
+
+// int main()
+// {
+//     int arr[] = {200, 11, 12, 56, 77};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     struct Pair minmax = getMinMax(arr, n);
+
+//     cout << "The largest number is " << minmax.max << endl;
+//     cout << "The smallest number is " << minmax.min << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
 
 struct Pair
 {
-    int max;
-    int min;
+    int min ;
+    int max ;
 };
 
-Pair getMinMax(int arr[], int size)
-{
+Pair getMinMax (int arr[], int size)
+{   
     struct Pair minmax;
-    if (size == 1)
+    if(size == 1)
     {
         minmax.max = arr[0];
         minmax.min = arr[0];
         return minmax;
     }
 
-    if (arr[0] > arr[1])
-    {
-        minmax.max = arr[0];
-        minmax.min = arr[1];
-    }
-    else if (arr[0] < arr[1])
-    {
-        minmax.max = arr[1];
-        minmax.min = arr[0];
-    }
-    for (int i = 2; i < size; i++)
-    {
-        if (arr[i] > minmax.max)
+        if(arr[0]>arr[1])
         {
-            minmax.max = arr[i];
+            minmax.max = arr[0];
+            minmax.min = arr[1];
         }
-        else if(arr[i] < minmax.min)
+        else
         {
-            minmax.min = arr[i];
+            minmax.max = arr[1];
+            minmax.min = arr[0];
         }
-    }
-    return minmax;
+
+        for(int i = 2; i < size; i++)
+        {
+            if(arr[i] > minmax.max)
+            {
+                minmax.max = arr[i];   
+            }
+            else if(arr[i] < minmax.min)
+            {
+                minmax.min = arr[i];
+            }
+        }
+        return minmax;
+    
+
 }
 
-int main()
+int main ()
 {
-    int arr[] = {200, 11, 12, 56, 77};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {10, 11, 800, 26, 26000, 42};
+    int n = sizeof(arr)/ sizeof(arr[0]);
 
-    struct Pair minmax = getMinMax(arr, n);
+    struct Pair minmax = getMinMax (arr, n);
 
-    cout << "The largest number is " << minmax.max << endl;
-    cout << "The smallest number is " << minmax.min << endl;
-
-    return 0;
+    cout<< "The largest number in the array is "<< minmax.max << endl;
+    cout<< "The smallest number in the array is "<< minmax.min << endl;
 }
--- 2. Find the maximum and minimum number in an array/tournamentMethod.cpp/findTheMaxMinOfAnArray.cpp
+++ 2. Find the maximum and minimum number in an array/tournamentMethod.cpp/findTheMaxMinOfAnArray.cpp
@@ -75,73 +75,148 @@
 //     return 0;
 // }
 
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// struct Pair 
+// {
+//     int max;
+//     int min;
+// };
+
+// Pair getMinMax (int arr[], int low, int high)
+// {
+//     int mid;
+//     struct Pair minmax, mml, mmr;
+//     //if the number of elements is equal to 1
+//     if(high == low)
+//     {
+//         minmax.max = arr[low];
+//         minmax.min = arr[low];
+//         return minmax;
+//     }
+//     //if the number of elements are equal to 2
+//     if(high == low + 1)
+//     {
+//         if(arr[low]> arr[high])
+//         {
+//             minmax.max = arr[low];
+//             minmax.min = arr[high]; 
+//         }
+//         else if(arr[low]< arr[high])
+//         {
+//             minmax.max = arr[high];
+//             minmax.max = arr[low];
+//         }
+//         return minmax;
+//     }
+//     //if number of elements are more than 2
+//     mid = (high+low)/2;
+//     mml = getMinMax(arr, low, mid);
+//     mmr = getMinMax(arr, mid+1, high);
+//     if(mml.max > mmr.max)
+//     {
+//         minmax.max = mml.max;
+//     }
+//     else
+//     {
+//         minmax.max = mmr.max;
+//     }
+//     if(mml.min < mmr.min){
+//         minmax.min =mml.min;
+//     }
+//     else
+//     {
+//         minmax.min = mmr.min;
+//     }
+//     return minmax;
+
+// }
+
+// int main ()
+// {
+//     int arr[] = {1000, 11, 445, 1, 330, 3000};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+
+//     struct Pair minmax = getMinMax(arr, 0, n -1);
+
+//     cout<<"The largest number is "<<minmax.max<<endl;
+//     cout<<"The smallest number is "<<minmax.min<<endl;
+//     return 0;
+// }
+
+
+#include <iostream>
 using namespace std;
 
-struct Pair 
+struct Pair
 {
     int max;
     int min;
 };
 
-Pair getMinMax (int arr[], int low, int high)
+Pair getMinMax(int arr[0], int low, int high)
 {
     int mid;
     struct Pair minmax, mml, mmr;
-    //if the number of elements is equal to 1
     if(high == low)
     {
-        minmax.max = arr[low];
         minmax.min = arr[low];
+        minmax.max = arr[low];
         return minmax;
     }
-    //if the number of elements are equal to 2
+
     if(high == low + 1)
     {
-        if(arr[low]> arr[high])
+        if(arr[high]>arr[low])
         {
-            minmax.max = arr[low];
-            minmax.min = arr[high]; 
-        }
-        else if(arr[low]< arr[high])
-        {
+            minmax.min = arr[low];
             minmax.max = arr[high];
+        }
+        else if(arr[high]<arr[low])
+        {
+            minmax.min = arr[high];
             minmax.max = arr[low];
         }
         return minmax;
     }
-    //if number of elements are more than 2
-    mid = (high+low)/2;
+
+    mid = (high + low) /2;
     mml = getMinMax(arr, low, mid);
     mmr = getMinMax(arr, mid+1, high);
+
     if(mml.max > mmr.max)
     {
         minmax.max = mml.max;
     }
-    else
+    else if(mml.max < mmr.max)
     {
         minmax.max = mmr.max;
     }
-    if(mml.min < mmr.min){
-        minmax.min =mml.min;
-    }
-    else
+
+    if(mml.min > mmr.min)
     {
         minmax.min = mmr.min;
+    }
+    else if(mml.min < mmr.min) 
+    {
+        minmax.min = mml.min;
     }
     return minmax;
 
 }
 
-int main ()
-{
-    int arr[] = {1000, 11, 445, 1, 330, 3000};
+int main (){
+
+    int arr[]= {10,11,800,26,26000,42};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    struct Pair minmax = getMinMax(arr, 0, n -1);
+    struct Pair minmax = getMinMax(arr, 0 , n-1);
 
-    cout<<"The largest number is "<<minmax.max<<endl;
-    cout<<"The smallest number is "<<minmax.min<<endl;
+    cout<<"The largest number is "<< minmax.max << endl;
+    cout<< "The smallest number is "<< minmax.min << endl;
+
     return 0;
+
 }
